87bde003b0f5a0c257af0e2d0f0701d490567926
Cyber Chef <40621362+ChefdePi@users.noreply.github.com>
2025-05-10 20:57:50 +1000

    pool is configured - removed require .env

--- db.js
+++ db.js
@@ -1,16 +1,13 @@
 // db.js
-require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host:               process.env.MYSQL_HOST,
-  user:               process.env.MYSQL_USER,
-  password:           process.env.MYSQL_PASSWORD,
-  database:           process.env.MYSQL_DB,
-  port:               parseInt(process.env.MYSQL_PORT, 10) || 3306,
-  waitForConnections: true,
-  connectionLimit:    10,
-  ssl:                { rejectUnauthorized: true }
+  host:     process.env.MYSQL_HOST,
+  user:     process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  port:     Number(process.env.MYSQL_PORT) || 3306,
+  ssl:      { rejectUnauthorized: false }
 });
 
 module.exports = pool;
